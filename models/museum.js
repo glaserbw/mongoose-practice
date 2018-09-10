a74982d6cfe34b3d73687a1a507f515f70f97bbc
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-09-10 16:41:24 -0700

    working on updating schemas

--- models/museum.js
+++ models/museum.js
@@ -6,3 +6,16 @@ const mongoose = require('mongoose');
 // TODO: Use schema to create model
 
 // TODO: Export Museum Model
+
+// Create the Museum Schema 
+const museumSchema = new mongoose.Schema({
+	name: String, 
+	image: String,  
+	city: String, 
+	country: String
+});
+
+// Create and export the user model
+module.exports = mongoose.model('Museum', museumSchema);
+
+
--- models/piece.js
+++ models/piece.js
@@ -14,3 +14,22 @@ const mongoose = require('mongoose');
 // NOTE: You don't need to worry about Creator schema. You don't need to
 // create a model for it or export it. This is because it lives inside
 // the Piece model, so that takes care of it all! Yay for embedded schemas!
+
+const creatorSchema = new mongoose.Schema({
+	firstname: String, 
+	lastname: String, 
+	image: String, 
+	birthyear: Number, 
+	deathyear: Number
+})
+
+const pieceSchema = new mongoose.Schema({
+	name: String, 
+	image: String,
+	creator: [creatorSchema]
+})
+
+module.exports = mongoose.model('Piece', pieceSchema);
+
+
+
