27e129cfc5d98a6186b43cec5bacc96f8f534f9e
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-09-10 16:56:33 -0700

    model schemas hopefully set up

--- models/index.js
+++ models/index.js
@@ -7,3 +7,10 @@ const mongoose = require('mongoose');
 
 // NOTE: Your files can have multiple module.exports statements!
 // Make sure to export both your Museum and Piece models!
+
+// Connect to your Mongoose database
+mongoose.connect(process.env.MONGOOSE_URL || 'mongodb://localhost:27017/mongoosemuseum', { useNewUrlParser: true });
+
+// Gather up your models 
+module.exports.Museum = require('./Museum');
+module.exports.Piece = require('./Piece');
